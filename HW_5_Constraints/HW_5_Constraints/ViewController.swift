e1789ee3faa273542b013adde8461566d53a1078
glbrom <golbrm@gmail.com>
2022-09-05 21:02:23 +0300

    HW-5-Constraints

---
+++ HW_5_Constraints/HW_5_Constraints/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_5_Constraints
+//
+//  Created by Macbook on 12.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
